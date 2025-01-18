fe8376263c93960664371e1015732f504180cc28
animex789 <animex0021@gmail.com>
2025-01-19 04:27:37 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -112,9 +112,7 @@ User - {}"""
 📆 Year: {release_date}
 ⭐ Rating: {rating} / 10</b>
 """
-
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/Jisshu_Originals">𝙅𝙞𝙨𝙨𝙝𝙪 𝙊𝙧𝙞𝙜𝙞𝙣𝙖𝙡𝙨</a>」</b>"""
-    
+FILE_CAPTION = """<b>{file_name}</b>"""
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
 
