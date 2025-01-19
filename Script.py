6fa1b493a8bc390b38a4a074666f92b14cd6271f
animex789 <animex0021@gmail.com>
2025-01-20 01:08:31 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -106,7 +106,7 @@ User - {}"""
 
 🎁 ʀᴇǫᴜᴇꜱᴛ ᴍꜱɢ - <code>{}</code></b>"""  
    
-    IMDB_TEMPLATE_TXT = """
+    IMDB_TEMPLATE_TXT = """<b>backupzone\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/Xbackupzonez">Xbackupzonez</a>」</b>"""
 <b>ʜᴇʏ {message.from_user.mention}, ʜᴇʀᴇ ɪꜱ ᴛʜᴇ ʀᴇꜱᴜʟᴛꜱ ꜰᴏʀ ʏᴏᴜʀ ǫᴜᴇʀʏ {search}.
 
 🍿 Title: {title}
